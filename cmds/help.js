4076763d3cbdc3e95ea7234ded421fe588999b7e
Tyler Flowers <gamingguycontact@gmail.com>
2020-02-26 23:48:25 +0000

    Transfer help message to another file

--- cmds/help.js
+++ cmds/help.js
@@ -1,36 +1,25 @@
 /**
  * THIS CODE WAS MADE FOR THE DSA DISCORD BOT AND CAN BE REUSED FOR ANY PURPOSE WITHOUT CREDIT. FOR FULL LEGAL AND LICENSING DISCLAIMERS, PLEASE READ LEGAL.TXT.
  * 
- * Help command. DM's the user an embedded message containing all the commands.
+ * Help command. DM's the user an embedded message containing all the commands. 
+ * 
+ * Embed message comes from a seperate file known as help.json, which can be found in the same folder as the one this is in.
  * 
  * ~~~developed by ggtylerr~~~
  */
 
+const fs = require('fs');
+
 exports.run = (client, message, args) => {
+  // Grab help JSON
+  var embed = JSON.parse(fs.readFileSync('./cmds/help.json'));
+
+  // Update custom values
+  embed.embed.author.name = client.user.username;
+  embed.embed.author.icon_url = client.user.avatarURL;
+  embed.embed.description = "Set prefix for this server: " + args[args.length-1];
+
+  // Send messages  
+  message.author.send(embed);
   message.channel.send('Check your DMs!')
-  message.author.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: 'Here\'s the commands for this bot!',
-    description: 'Set prefix for this server: ' + args[args.length-1],
-    fields: [{
-        name: 'General Commands',
-        value: 'help: Displays this message.\nsayhelp: Displays this message, but sends it in the channel instead of DM\'ing you it.\ncount: Shows the # of msgs that has been sent since this bot existed (or 5 minutes after the count was implemented)\nrng (num): Chooses a random number from 1 thru (num)\nvote (msg): Makes a vote message.\nyorn (msg): Answers a yes/no question.\nsend (msg): Makes me send a message. @ becomes italicized to prevent pings.\nflip: Flip a coin.\nrngcase (msg): Randomize the case of a message.'
-      },
-      {
-        name: 'Meme Commands',
-        value: 'ping: Pong.\nf: Presses [F] to pay respects.\nbruh: Certifies a bruh moment.\ngeico: Save 15% or more on car insurance.\ngold: Give reddit gold.\ncoughcount: Shows the # of coughs (including the ones by the bot)\noof: OOF\ncum: COMSUME THE CUM CHALICE'
-      },
-      {
-        name: 'Admin Only Commands',
-        value: '*These commands require you to have the administrator permission on your server*\nprefix (prefix): Sets a new prefix for the server.'
-      },
-      {
-        name: 'No Prefix',
-        value: 'ayy: lmao\nowo: What\'s this?\nCough: Cough\nme2 *or* me too *or* me to: me too\nWhenever WigglesJ sends a message: me too'
-      }]
-  }});
 }
