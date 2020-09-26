50571099020ba1142f4263c76b9af84f7bc7bc7f
Tyler Flowers <ggtylerr_contact@protonmail.com>
2020-09-26 10:12:02 +0000

    Add url testing and proper admin only functionality

--- cmds/sggqu/smashqueue.js
+++ cmds/sggqu/smashqueue.js
@@ -1,7 +1,7 @@
 /**
  * THIS CODE WAS MADE FOR THE DSA DISCORD BOT AND CAN BE REUSED FOR ANY PURPOSE WITHOUT CREDIT. FOR FULL LEGAL AND LICENSING DISCLAIMERS, PLEASE READ LEGAL.TXT.
  * 
- * Cough timeout command. Utilizes quick.db to have server-specific cough timeouts.
+ * smash.gg queue command. Utilizes node-json-db to make channel specific queues.
  * 
  * ~~~developed by ggtylerr~~~
  */
@@ -10,18 +10,20 @@ const Commando = require('discord.js-commando');
 
 const JsonDB = require('node-json-db').JsonDB;
 const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
+const {GraphQLClient} = require('graphql-request');
 
 var serverDB = new JsonDB(new Config(process.env.appRoot + "/db/serverDB",false,true,'/'));
 var channelDB = new JsonDB(new Config(process.env.appRoot + "/db/channelDB",false,true,'/'));
 
-module.exports = class CoughTimeoutCommand extends Commando.Command {
+module.exports = class SmashGGQueueCommand extends Commando.Command {
   constructor(client) {
     super(client, {
       name: 'smashqueue',
+      aliases: ['squeue','sq'],
       group: 'sggqu',
       memberName: 'smashqueue',
       description: 'Queues up a tournament or league to be ready for commands.',
-      details: 'Can be set to admin only using the command (not implemented yet)',
+      details: 'Can be set to admin only using the command `smashqueueadmin`',
       examples: ['smashqueue silver-state-smash-3'],
       args: [
         {
@@ -37,18 +39,47 @@ module.exports = class CoughTimeoutCommand extends Commando.Command {
     serverDB.load();
     channelDB.load();
     // Check if the command should only be run by admins
-    var adminOnly = false;
     var id = message.channel.guild.id;
-    // TODO: Implement admin only functionality and command.
-    if (adminOnly && !message.member.hasPermission('ADMINISTRATOR')) {
+    var dbd8a = false;
+    try {
+      dbd8a = serverDB.getData(`/${id}/smashqueueadmin`);
+    } catch (e) {
+      // Not set as admin only, ignore.
+    }
+    if (dbd8a && !message.member.hasPermission('ADMINISTRATOR')) {
       return message.reply('This command can only be used by people have who admin perms!');
     }
-    // TODO: Idiot proof links
+    // TODO: Add URL to slug conversion
+    // Make sure links work
+    const { Headers } = require('cross-fetch');
+    global.Headers = global.Headers || Headers;
+    const GQLClient = new GraphQLClient("https://api.smash.gg/gql/alpha", {
+      headers: {
+        authorization: `Bearer ${process.env.smashggapi}`
+      }
+    });
+    // Test if it's a tournament
+    const query = "query x($s:String){tournament(slug:$s){name}}";
+    const vars = {s:slug};
+    const data = await GQLClient.request(query, vars);
+    const type = "t";
+    if (data.tournament === null) {
+      // Test if it's a league
+      query = "query x($s:String){league(slug:$s){name}}"
+      data = await GQLClient.request(query, vars);
+      type = "l";
+      if (data.league === null) {
+        // Slug failed both tests
+        return message.reply('This tournament/league/url doesn\'t work.');
+      }
+    }
     // Push to channel database
     var cid = message.channel.id;
     channelDB.push(`/${cid}/sq/link`,slug);
-    channelDB.push(`/${cid}/sq/type`,null);
+    channelDB.push(`/${cid}/sq/type`,type);
     channelDB.save();
-    message.channel.send(`Queue updated! New queued tourney: ${slug}`);
+    var displayType = "tourney";
+    if (type == "l") displayType = "league";
+    message.channel.send(`Queue updated! New queued ${displayType}: ${data.tournament.name}`);
   }
 }
