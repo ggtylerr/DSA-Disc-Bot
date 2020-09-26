0a01bf9c033955cd3a65d2d03e48c701b3b1f7b3
Tyler Flowers <ggtylerr_contact@protonmail.com>
2020-09-26 12:11:09 +0000

    Add actual commands to use queues

---
+++ cmds/sggqu/smashqueuetourney.js
@@ -0,0 +1,65 @@
+/**
+ * THIS CODE WAS MADE FOR THE DSA DISCORD BOT AND CAN BE REUSED FOR ANY PURPOSE WITHOUT CREDIT. FOR FULL LEGAL AND LICENSING DISCLAIMERS, PLEASE READ LEGAL.TXT.
+ * 
+ * Queued tourney info command. Gets information on a queued tournament hosted on smash.gg.
+ * 
+ * ~~~developed by ggtylerr~~~
+ */
+
+const Commando = require('discord.js-commando');
+const {GraphQLClient} = require('graphql-request');
+const fs = require('fs');
+const {tourney} = require('../../util/smash/embedgen');
+const JsonDB = require('node-json-db').JsonDB;
+const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
+
+var channelDB = new JsonDB(new Config(process.env.appRoot + "/db/channelDB",false,true,'/'));
+
+module.exports = class SmashGGQueueTourneyCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'smashggqueuetourney',
+      aliases: ['squeuetourney','sqt'],
+      group: 'smash',
+      memberName: 'smashggqueuetourney',
+      description: 'Get info from a queued tournament.',
+      details: 'Does not display information on events, phases, etc.'
+    });
+  }
+  async run(message) {
+    // Load databases
+    channelDB.load();
+    // Get queue data
+    var id = message.channel.id;
+    var q = null;
+    try {
+      q = channelDB.getData(`/${id}/sq`);
+    } catch (e) {
+      return message.channel.send("There is nothing in the queue.");
+    }
+    // Cancel if the queued item isn't a tourney
+    if (q.type !== "t") {
+      return message.channel.send("The current queued item is not a tourney!");
+    }
+    // Init client
+    const { Headers } = require('cross-fetch');
+    global.Headers = global.Headers || Headers;
+    
+    const GQLClient = new GraphQLClient("https://api.smash.gg/gql/alpha", {
+      headers: {
+        authorization: `Bearer ${process.env.smashggapi}`
+      }
+    })
+
+    // Set query and vars
+    const query = fs.readFileSync('././util/smash/schema/tourney.gql', 'utf8');
+    const vars = {slug:q.link};
+
+    // Get response
+    const data = await GQLClient.request(query, vars);
+    const d = data.tournament;
+    
+    // Generate and send embed
+    message.channel.send(tourney(d,q.link,this.client.user));
+  }
+}
